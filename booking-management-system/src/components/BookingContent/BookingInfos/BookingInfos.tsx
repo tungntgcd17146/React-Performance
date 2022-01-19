caf4d554e382856bfccf489e243d24163b52c0a3
Tung Thanh Nguyen <tung.nguyen@asnet.com.vn>
2022-01-19 22:47:09 +0700

    feat: create feature render booking infos from API

--- booking-management-system/src/components/BookingContent/BookingInfos/BookingInfos.tsx
+++ booking-management-system/src/components/BookingContent/BookingInfos/BookingInfos.tsx
@@ -1,221 +1,76 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useBookInfo } from '../../../utils/hooks/hooks';
+import { fetchInfos } from '../../../reducer/bookingContent/actions';
+
+import api from '../../../api/index';
 
 import style from './BookingInfos.module.css';
 
+
 export const BookingInfos = () => {
+
+  const [state, dispatch] = useBookInfo();
+
+  //Retrieve Booking infos category
+  const retrieveInfos = async () => {
+    const response = await api.get('/bookingInfos');
+    return response.data;
+  };
+
+  useEffect(() => {
+    const getRoomCategory = async () => {
+      const allRoom = await retrieveInfos();
+      if (allRoom) {
+        dispatch(fetchInfos(allRoom));
+      }
+    };
+    getRoomCategory();
+  }, []);
+
+  console.log(state)
+
+  const { byId, allIds } = state;
+
+  const renderBookingInfos = allIds.map((id: Number, index) => {
+    
+    return (
+      <div className="col-4 mb-3" key={index}>
+        <div className={`card ${style.cardHeight}`}>
+          <img
+            src="../../../../public/images/deluxe-king-1.jpg"
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">Name: {byId[id].name}</h5>
+            <p className={`card-text mb-0 ${style.emailHeight}`}>Email: {byId[id].email}</p>
+            <p className="card-text mb-0">Phone: {byId[id].phone}</p>
+            <small className="text-muted">Check-in: {byId[id].checkIn}</small>
+            <p className="card-text">
+                <small className="text-muted">Check-out: {byId[id].checkOut}</small>
+            </p>
+            <p className="card-text mb-0">Total Price: {byId[id].totalPrice}</p>
+            <p className="card-text mb-0">Total Room: {byId[id].roomNumber}</p>
+            <p className="card-text">
+                <small className="text-muted">Room Category: {byId[id].roomName}</small>
+            </p>
+            <div className='d-flex justify-content-between'>
+              <a href="#" className="btn btn-outline-primary col-5">
+                Edit
+              </a>
+              <a href="#" className="btn btn-outline-danger col-5">
+                Delete
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  });
+  
   return (
     <div className={`row mt-5 ${style.color} ${style.heightContent}`}>
-      <div className="col-4 mb-3">
-        <div className="card">
-          <img
-            src="../../../../public/images/deluxe-king-1.jpg"
-            className="card-img-top"
-            alt="..."
-          />
-          <div className="card-body">
-            <h5 className="card-title">Customer Name:</h5>
-            <p className="card-text mb-0">Email:</p>
-            <p className="card-text mb-0">Phone::</p>
-            <small className="text-muted">Check-in:</small>
-            <p className="card-text">
-                <small className="text-muted">Check-out:</small>
-            </p>
-            <p className="card-text mb-0">Total Price::</p>
-            <p className="card-text">
-                <small className="text-muted">Room Category:</small>
-            </p>
-            <div className='d-flex justify-content-between'>
-              <a href="#" className="btn btn-outline-primary col-5">
-                Edit
-              </a>
-              <a href="#" className="btn btn-outline-danger col-5">
-                Delete
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="col-4 mb-3">
-        <div className="card">
-          <img
-            src="../../../../public/images/deluxe-king-1.jpg"
-            className="card-img-top"
-            alt="..."
-          />
-          <div className="card-body">
-            <h5 className="card-title">Customer Name:</h5>
-            <p className="card-text mb-0">Email:</p>
-            <p className="card-text mb-0">Phone::</p>
-            <small className="text-muted">Check-in:</small>
-            <p className="card-text">
-                <small className="text-muted">Check-out:</small>
-            </p>
-            <p className="card-text mb-0">Total Price::</p>
-            <p className="card-text">
-                <small className="text-muted">Room Category:</small>
-            </p>
-            <div className='d-flex justify-content-between'>
-              <a href="#" className="btn btn-outline-primary col-5">
-                Edit
-              </a>
-              <a href="#" className="btn btn-outline-danger col-5">
-                Delete
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="col-4 mb-3">
-        <div className="card">
-          <img
-            src="../../../../public/images/deluxe-king-1.jpg"
-            className="card-img-top"
-            alt="..."
-          />
-          <div className="card-body">
-            <h5 className="card-title">Customer Name:</h5>
-            <p className="card-text mb-0">Email:</p>
-            <p className="card-text mb-0">Phone::</p>
-            <small className="text-muted">Check-in:</small>
-            <p className="card-text">
-                <small className="text-muted">Check-out:</small>
-            </p>
-            <p className="card-text mb-0">Total Price::</p>
-            <p className="card-text">
-                <small className="text-muted">Room Category:</small>
-            </p>
-            <div className='d-flex justify-content-between'>
-              <a href="#" className="btn btn-outline-primary col-5">
-                Edit
-              </a>
-              <a href="#" className="btn btn-outline-danger col-5">
-                Delete
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="col-4 mb-3">
-        <div className="card">
-          <img
-            src="../../../../public/images/deluxe-king-1.jpg"
-            className="card-img-top"
-            alt="..."
-          />
-          <div className="card-body">
-            <h5 className="card-title">Customer Name:</h5>
-            <p className="card-text mb-0">Email:</p>
-            <p className="card-text mb-0">Phone::</p>
-            <small className="text-muted">Check-in:</small>
-            <p className="card-text">
-                <small className="text-muted">Check-out:</small>
-            </p>
-            <p className="card-text mb-0">Total Price::</p>
-            <p className="card-text">
-                <small className="text-muted">Room Category:</small>
-            </p>
-            <div className='d-flex justify-content-between'>
-              <a href="#" className="btn btn-outline-primary col-5">
-                Edit
-              </a>
-              <a href="#" className="btn btn-outline-danger col-5">
-                Delete
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="col-4 mb-3">
-        <div className="card">
-          <img
-            src="../../../../public/images/deluxe-king-1.jpg"
-            className="card-img-top"
-            alt="..."
-          />
-          <div className="card-body">
-            <h5 className="card-title">Customer Name:</h5>
-            <p className="card-text mb-0">Email:</p>
-            <p className="card-text mb-0">Phone::</p>
-            <small className="text-muted">Check-in:</small>
-            <p className="card-text">
-                <small className="text-muted">Check-out:</small>
-            </p>
-            <p className="card-text mb-0">Total Price::</p>
-            <p className="card-text">
-                <small className="text-muted">Room Category:</small>
-            </p>
-            <div className='d-flex justify-content-between'>
-              <a href="#" className="btn btn-outline-primary col-5">
-                Edit
-              </a>
-              <a href="#" className="btn btn-outline-danger col-5">
-                Delete
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="col-4 mb-3">
-        <div className="card">
-          <img
-            src="../../../../public/images/deluxe-king-1.jpg"
-            className="card-img-top"
-            alt="..."
-          />
-          <div className="card-body">
-            <h5 className="card-title">Customer Name:</h5>
-            <p className="card-text mb-0">Email:</p>
-            <p className="card-text mb-0">Phone::</p>
-            <small className="text-muted">Check-in:</small>
-            <p className="card-text">
-                <small className="text-muted">Check-out:</small>
-            </p>
-            <p className="card-text mb-0">Total Price::</p>
-            <p className="card-text">
-                <small className="text-muted">Room Category:</small>
-            </p>
-            <div className='d-flex justify-content-between'>
-              <a href="#" className="btn btn-outline-primary col-5">
-                Edit
-              </a>
-              <a href="#" className="btn btn-outline-danger col-5">
-                Delete
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="col-4 mb-3">
-        <div className="card">
-          <img
-            src="../../../../public/images/deluxe-king-1.jpg"
-            className="card-img-top"
-            alt="..."
-          />
-          <div className="card-body">
-            <h5 className="card-title">Customer Name:</h5>
-            <p className="card-text mb-0">Email:</p>
-            <p className="card-text mb-0">Phone::</p>
-            <small className="text-muted">Check-in:</small>
-            <p className="card-text">
-                <small className="text-muted">Check-out:</small>
-            </p>
-            <p className="card-text mb-0">Total Price::</p>
-            <p className="card-text">
-                <small className="text-muted">Room Category:</small>
-            </p>
-            <div className='d-flex justify-content-between'>
-              <a href="#" className="btn btn-outline-primary col-5">
-                Edit
-              </a>
-              <a href="#" className="btn btn-outline-danger col-5">
-                Delete
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-      
+      {renderBookingInfos}
     </div>
   );
 };
--- booking-management-system/src/reducer/bookingContent/reducer.ts
+++ booking-management-system/src/reducer/bookingContent/reducer.ts
@@ -1,4 +1,4 @@
-import { FETCH_ROOM, ADD_ROOM, DELETE_ROOM, EDIT_ROOM } from '../../constants/roomCategory';
+import { FETCH_INFO } from '../../constants/bookingInfos';
 import { convertArrayToObject } from '../../utils/helper/helper';
 
 //Init room category
@@ -10,38 +10,38 @@ export const InitInfos = {
 //Reducer
 const reducer = (state, action) => {
   switch (action.type) {
-    case FETCH_ROOM:
-      const rooms = action.payload;
-      const newById = convertArrayToObject(rooms, 'id');
-      const newIds = rooms.map((room) => room.id);
+    case FETCH_INFO:
+      const infos = action.payload;
+      const newById = convertArrayToObject(infos, 'id');
+      const newIds = infos.map((room) => room.id);
 
       return {
         byId: newById,
         allIds: newIds
       };
 
-    case ADD_ROOM:
-      const newRoom = action.payload;
-      state.byId[state.allIds.length + 1] = newRoom;
+    // case ADD_ROOM:
+    //   const newRoom = action.payload;
+    //   state.byId[state.allIds.length + 1] = newRoom;
 
-      return {
-        byId: {
-          ...state.byId
-        },
-        allIds: [...state.allIds, state.allIds.length + 1]
-      };
+    //   return {
+    //     byId: {
+    //       ...state.byId
+    //     },
+    //     allIds: [...state.allIds, state.allIds.length + 1]
+    //   };
 
-    case DELETE_ROOM:
-      const roomId = action.payload;
-      delete state.byId[roomId];
-      state.allIds.splice(roomId - 1, 1);
+    // case DELETE_ROOM:
+    //   const roomId = action.payload;
+    //   delete state.byId[roomId];
+    //   state.allIds.splice(roomId - 1, 1);
 
-      return {
-        byId: {
-          ...state.byId
-        },
-        allIds: [...state.allIds]
-      };
+    //   return {
+    //     byId: {
+    //       ...state.byId
+    //     },
+    //     allIds: [...state.allIds]
+    //   };
 
     // case EDIT_ROOM:
     //   return {
