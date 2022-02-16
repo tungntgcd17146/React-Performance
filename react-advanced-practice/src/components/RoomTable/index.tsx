6c155fc1e1b77b540d52c7a8163b733e40d1ed0a
Tung Thanh Nguyen <tung.nguyen@asnet.com.vn>
2022-02-16 16:51:25 +0700

    feat: create error boundary to show error when clear all list

--- react-advanced-practice/src/components/RoomTable/index.tsx
+++ react-advanced-practice/src/components/RoomTable/index.tsx
@@ -3,11 +3,32 @@ import Room from './Room';
 import SortButton from './SortButton';
 import { RoomInterface } from '../../interface/room';
 
+import { ErrorBoundary } from 'react-error-boundary';
+
 type Props = {
   roomsAfterFilter: RoomInterface[];
   onDeleteRoom: (roomId: string) => void;
   onSortButton: () => void;
   toggleSort: Boolean;
+};
+
+type ErrorFallbackProps = {
+  error: Error;
+  resetErrorBoundary: () => void;
+};
+
+const ErrorFallback = ({ error, resetErrorBoundary }: ErrorFallbackProps) => {
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>Try again</button>
+    </div>
+  );
+};
+
+const Bomb = () => {
+  throw new Error('Do not have any list!!');
 };
 
 export const RoomTable = ({ roomsAfterFilter, onDeleteRoom, onSortButton, toggleSort }: Props) => (
@@ -32,9 +53,15 @@ export const RoomTable = ({ roomsAfterFilter, onDeleteRoom, onSortButton, toggle
       </tr>
     </thead>
     <tbody className="table-hover">
-      {roomsAfterFilter.map((room: RoomInterface, index: number) => (
-        <Room key={room.id} room={room} order={index + 1} onDelete={onDeleteRoom} />
-      ))}
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        {roomsAfterFilter.length === 0 ? (
+          <Bomb />
+        ) : (
+          roomsAfterFilter.map((room: RoomInterface, index: number) => (
+            <Room key={room.id} room={room} order={index + 1} onDelete={onDeleteRoom} />
+          ))
+        )}
+      </ErrorBoundary>
     </tbody>
   </table>
 );
