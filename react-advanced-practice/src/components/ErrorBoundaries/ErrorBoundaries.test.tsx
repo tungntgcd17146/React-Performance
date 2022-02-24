58aadf3db643a9c0681b2acb4307b4a1f2650de8
Tung Thanh Nguyen <tung.nguyen@asnet.com.vn>
2022-02-24 17:05:44 +0700

    fix: update write test for ErrorBoundaries component

--- react-advanced-practice/src/components/ErrorBoundaries/ErrorBoundaries.test.tsx
+++ react-advanced-practice/src/components/ErrorBoundaries/ErrorBoundaries.test.tsx
@@ -3,20 +3,18 @@ import React from 'react';
 import ErrorBoundary from './index';
 import { render, screen } from '@testing-library/react';
 
+const ThrowError = () => {
+  throw new Error('Test error');
+};
+
 describe('Error Boundary', () => {
   test('Error Boundary', () => {
-    const ThrowError = () => {
-      throw new Error('Test error');
-    };
-
     render(
       <ErrorBoundary>
         <ThrowError />
       </ErrorBoundary>
     );
 
-    const errorMessage = screen.getByTestId('errorboundary');
-
-    expect(errorMessage.textContent).toBe('Something went wrong!');
+    expect(screen.getByTestId('errorboundary')).toBeVisible();
   });
 });
