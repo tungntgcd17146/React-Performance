9d2e46a33ed8696d235a9243048810dfb0a2921f
Tung Nguyen <tung.nguyen@asnet.com.vn>
2024-01-10 14:57:16 +0700

    more setup

--- revise-react/example-project/.eslintrc.cjs
+++ revise-react/example-project/.eslintrc.cjs
@@ -2,34 +2,30 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
-    "eslint-config-prettier",
-    "prettier",
-    "plugin:storybook/recommended"
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'eslint-config-prettier',
+    'prettier',
+    'plugin:storybook/recommended'
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs", "vite.config.ts"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh", "prettier"],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'prettier'],
   rules: {
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
-    "prettier/prettier": [
-      "warn",
+    'prettier/prettier': [
+      'warn',
       {
-        arrowParens: "always",
+        arrowParens: 'always',
         semi: false,
-        trailingComma: "none",
+        trailingComma: 'none',
         tabWidth: 2,
-        endOfLine: "auto",
+        endOfLine: 'auto',
         useTabs: false,
         singleQuote: true,
         printWidth: 120,
-        jsxSingleQuote: true,
-      },
-    ],
-  },
-};
+        jsxSingleQuote: true
+      }
+    ]
+  }
+}
