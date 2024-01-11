88613fbee7dda35ea7e7cdba274798af0076046c
Tung Nguyen <tung.nguyen@asnet.com.vn>
2024-01-11 11:20:42 +0700

    example with redirect, dataLoader, and pedding UI when direct route

--- revise-react/example-react-router/src/routes/root.jsx
+++ revise-react/example-react-router/src/routes/root.jsx
@@ -1,6 +1,26 @@
-import { Outlet, Link } from "react-router-dom";
+import {
+  Outlet,
+  NavLink,
+  useLoaderData,
+  Form,
+  useNavigation,
+} from "react-router-dom";
+import { getContacts, createContact } from "../contacts";
+
+export async function loader() {
+  const contacts = await getContacts();
+  return { contacts };
+}
+
+export async function action() {
+  const contact = await createContact();
+  return { contact };
+}
 
 export default function Root() {
+  const { contacts } = useLoaderData();
+  const navigation = useNavigation();
+
   return (
     <>
       <div id='sidebar'>
@@ -17,22 +37,44 @@ export default function Root() {
             <div id='search-spinner' aria-hidden hidden={true} />
             <div className='sr-only' aria-live='polite'></div>
           </form>
-          <form method='post'>
+          <Form method='post'>
             <button type='submit'>New</button>
-          </form>
+          </Form>
         </div>
         <nav>
-          <ul>
-            <li>
-              <Link to={`contacts/1`}>Your Name</Link>
-            </li>
-            <li>
-              <Link to={`contacts/2`}>Your Friend</Link>
-            </li>
-          </ul>
+          {contacts.length ? (
+            <ul>
+              {contacts.map((contact) => (
+                <li key={contact.id}>
+                  <NavLink
+                    to={`contacts/${contact.id}`}
+                    className={({ isActive, isPending }) =>
+                      isActive ? "active" : isPending ? "pending" : ""
+                    }
+                  >
+                    {contact.first || contact.last ? (
+                      <>
+                        {contact.first} {contact.last}
+                      </>
+                    ) : (
+                      <i>No Name</i>
+                    )}{" "}
+                    {contact.favorite && <span>★</span>}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>
+              <i>No contacts</i>
+            </p>
+          )}
         </nav>
       </div>
-      <div id='detail'>
+      <div
+        id='detail'
+        className={navigation.state === "loading" ? "loading" : ""}
+      >
         <Outlet />
       </div>
     </>
