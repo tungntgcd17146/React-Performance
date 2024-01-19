27dbc5370bf0b6f303bc751efdba51ca6f80da63
Tung Nguyen <tung.nguyen@asnet.com.vn>
2024-01-19 14:17:31 +0700

    setup test with vitest

--- react-revise-practice/vite.config.ts
+++ react-revise-practice/vite.config.ts
@@ -1,19 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path'
+import * as path from 'path'
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }]
+  },
   server: {
     port: 3000
   },
   css: {
     devSourcemap: true
   },
-  resolve: {
-    alias: {
-      '~': path.resolve(__dirname, './src')
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    coverage: {
+      enabled: true,
+      provider: 'v8',
+      exclude: [
+        // Add patterns to exclude here, for example, to ignore Storybook files
+        '**/*.stories.@(js|jsx|ts|tsx)',
+        '**/*.mdx',
+        '**/node_modules/**',
+        '**/.storybook/**',
+        '**/.eslintrc.cjs',
+        '**/*.config.@(js|jsx|ts|tsx)',
+        '**/constants/**'
+      ]
     }
   }
 })
