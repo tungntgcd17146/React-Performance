1cb9e135505105473f1397837f356235febd71bd
Tung Nguyen <tung.nguyen@asnet.com.vn>
2024-01-25 15:56:17 +0700

    implement Avatar component

---
+++ react-revise-practice/src/components/Avatar/index.tsx
@@ -0,0 +1,79 @@
+import { memo } from 'react'
+
+import Badge from '@mui/material/Badge'
+import { AvatarProps, Avatar as MuiAvatar, styled } from '@mui/material'
+
+export interface Props extends AvatarProps {
+  src: string
+  alt: string
+  size: 'small' | 'medium' | 'large'
+  avtBackground?: string
+  BadgeIcon?: React.ReactNode
+  onClick?: () => void
+}
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  '& .MuiBadge-badge': {
+    border: `1px solid ${theme.palette.primary.main}`,
+    padding: 0,
+    margin: 0,
+    width: 24,
+    height: 24,
+    borderRadius: '100%',
+    backgroundColor: theme.palette.info.main,
+    color: theme.palette.primary.main,
+    '&::after': {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: '16px',
+      height: '16px',
+      borderRadius: '50%'
+    }
+  }
+}))
+
+const Avatar = ({ BadgeIcon, onClick, size = 'medium', avtBackground, alt, src, ...rest }: Props) => {
+  const getSize = (sizes?: 'small' | 'medium' | 'large') => {
+    switch (sizes) {
+      case 'small':
+        return '48px'
+      case 'medium':
+        return '64px'
+      case 'large':
+        return '80px'
+    }
+  }
+
+  if (!BadgeIcon) {
+    return (
+      <MuiAvatar
+        data-testid='Avatar_MuiAvatar'
+        onClick={onClick}
+        sx={{ width: getSize(size), height: getSize(size), backgroundColor: avtBackground }}
+        alt={alt}
+        src={src}
+        {...rest}
+      />
+    )
+  }
+
+  return (
+    <StyledBadge
+      data-testid='Avatar_StyledBadge'
+      overlap='circular'
+      anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+      badgeContent={BadgeIcon}
+      onClick={onClick}
+    >
+      <MuiAvatar
+        sx={{ width: getSize(size), height: getSize(size), backgroundColor: avtBackground }}
+        alt={alt}
+        src={src}
+        {...rest}
+      />
+    </StyledBadge>
+  )
+}
+
+export default memo(Avatar)
--- react-revise-practice/src/pages/Header/index.tsx
+++ react-revise-practice/src/pages/Header/index.tsx
@@ -4,20 +4,25 @@ import { useMode } from '@/contexts/modeContext/useModeContext'
 import useScreenWidth from '@/hooks/useScreenWidth'
 
 //mui
+import { useTheme } from '@mui/material'
 import DragHandleIcon from '@mui/icons-material/DragHandle'
 import SearchIcon from '@mui/icons-material/Search'
 import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline'
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone'
-import IconButton from '@mui/material/IconButton'
 
 //components
+import IconButton from '@/components/IconButton'
 import Drawer from '@/components/Drawer/'
+import Avatar from '@/components/Avatar'
+import Customer1 from '@/assets/customer1.png'
+import { themes } from '@/themes'
 
 const Header = () => {
   const [isOpenDrawer, setIsOpenDrawer] = useState(false)
 
   const { isDarkMode, toggleMode } = useMode()
   const { isMobile, isTablet, isDesktop } = useScreenWidth()
+  const theme = useTheme()
 
   const handleCloseDrawer = () => {
     setIsOpenDrawer(false)
@@ -50,8 +55,20 @@ const Header = () => {
 
       <div className='flex flex-row gap-[24px]'>
         <IconButton children={<SearchIcon />} size='large' />
-        <IconButton children={<ChatBubbleOutlineIcon />} size='large' />
-        <IconButton children={<NotificationsNoneIcon />} size='large' />
+        <IconButton
+          badgeContent={0}
+          children={<ChatBubbleOutlineIcon />}
+          size='large'
+          sx={{ ':hover': { color: theme.palette.text.secondary } }}
+        />
+        <IconButton
+          badgeContent={0}
+          children={<NotificationsNoneIcon />}
+          size='large'
+          sx={{ ':hover': { color: theme.palette.text.secondary } }}
+        />
+
+        <Avatar avtBackground={themes.colors.yellow[600]} size='small' src={Customer1} alt='Customer1' />
       </div>
     </Box>
   )
