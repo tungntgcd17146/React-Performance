2e3eba0549fdaf747957be5e5e1246adf29476dd
Tung Nguyen <tung.nguyen@asnet.com.vn>
2024-02-20 18:09:22 +0700

    fix storybook

--- react-revise-practice/src/components/ImageDrawer/ImageDrawer.stories.tsx
+++ react-revise-practice/src/components/ImageDrawer/ImageDrawer.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import DarkProductDetail from '@/assets/ProductDetailImgDark.jpg'
+import DarkProductDetail from '/assets/ProductDetailImgDark.jpg'
 
 import ImageDrawer from '.'
 
